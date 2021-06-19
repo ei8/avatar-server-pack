cf614fa1e16fb891e98837ff0fdaa76d1f490c4b
elmerbool <35117496+elmerbool@users.noreply.github.com>
2021-06-20 00:38:20 +1000

    Merged d23 and idp into sample.

--- start all.bat
+++ start all.bat
@@ -1,5 +1,3 @@
 start "" ngrok start -config ngrok.yml neurul idp-neurul d23-neurul
 start "" traefik -c traefik.toml
-start "" /d "c:\ei8\idp" docker-compose -f docker-compose.yml up
 start "" /d "c:\ei8\avatars\prod\sample" docker-compose -f docker-compose.yml up
-start "" /d "c:\ei8\d23" docker-compose -f docker-compose.yml up
